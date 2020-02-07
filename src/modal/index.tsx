d925a3f2e7a6e986d0a8c2ab3cbf2b9fd1cac973
Spencer Seeger <spencerseeger@Spencers-MacBook-Pro-2.local>
2020-02-06 19:28:15 -0600

    adding modal

---
+++ src/modal/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './modal.css';
+
+export type ModalProps = {
+    show: boolean,
+    onClick: Function
+}
+
+class Modal extends React.Component<ModalProps> {
+
+    constructor(props: ModalProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div style={{display: this.props.show ? '' : 'None'}} className='modal'>
+                <div className='modal-body'>
+                    {this.props.children}
+                </div>
+                <div className='modal-background' onClick={() => this.props.onClick()}></div>
+            </div>
+        );
+    }
+}
+
+export default Modal;
